1c4a5c4b28a467cf8a6f8e4e6dbb8bd90834d4e0
sistemassdeh <63413763+sistemassdeh@users.noreply.github.com>
2021-06-09 16:22:00 -0300

    Add files via upload

---
+++ l10n_ar_fe_qr/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'l10n_ar_fe_qr',
+    'version': '1.0',
+    'category': 'l10n_ar_fe_qr',
+    'summary': 'Plants and customers management',
+    'depends': ['account','l10n_ar','l10n_ar_afipws_fe'],
+    'data': [
+        'afip_view.xml',
+    ],
+    'demo': [
+        ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
